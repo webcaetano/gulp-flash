02fb2d11d173ae0263e9bcfe9ecb290284044a9f
unknown <dezaodoido@gmail.com>
2015-05-01 15:15:33 -0300

    try travis

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ module.exports = function (output,params) {
 		var args = [file.path]; // input
 
 		if(typeof output==='string') { // if string are output path
-			if (!fs.existsSync(path.dirname(path.resolve(output)))) fs.mkdirSync(path.dirname(path.resolve(output)));
-			args.push((fs.lstatSync(path.resolve(output)).isDirectory() ? path.resolve(output,path.basename(file.path,'.as')+'.swf') : path.resolve(output)));
+			args.push((path.extname(path.resolve(output))!='.as' ? path.resolve(output,path.basename(file.path,'.as')+'.swf') : path.resolve(output)));
 		}
 
 		if(typeof output==='object') { //params
